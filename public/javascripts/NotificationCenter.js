c25fdc88ae149175b6a70e21ad9a266a4cf79bc5
Hugo Tunius <hugo.tunius@gmail.com>
2013-11-06 09:20:14 +0100

    Remove global

---
+++ public/javascripts/NotificationCenter.js
@@ -0,0 +1,38 @@
+(function (win) {
+  "use strict";
+
+  if (typeof win.BMAP === "undefined") { win.BMAP = {}; }
+
+  win.BMAP.NotificationCenter = (function () {
+    var subscribers = {},
+
+      subscribe = function (event, callback) {
+        if (typeof subscribers[event] === "undefined") {
+          subscribers[event] = [];
+        }
+
+        subscribers[event].push(callback);
+      },
+
+      write = function (event, object) {
+        var i = 0,
+        events = subscribers[event],
+        current;
+
+        if (typeof events !== "undefined") {
+
+          for (i = events.length; i >= 0; i--) {
+            current = events[i];
+            if (typeof current === "function") {
+              current(object);
+            }
+          }
+        }
+      };
+
+      return {
+        subscribe: subscribe,
+        write: write
+      };
+  }());
+}(window));
--- public/javascripts/localPlaylist/index.js
+++ public/javascripts/localPlaylist/index.js
@@ -1,35 +1,20 @@
 (function(win, doc, $, undefined){
     "use strict";
 	var BMAP = win.BMAP || {};
-	
+
 	//the first to run when document is ready
     $(function(){
-		var templateEl = generateTemplate();
-		var resultEl       = $("#overlay-search-results");
-		
-		BMAP.YoutubePlayer = new BMAP.YoutubePlayer(280,200);
-		BMAP.VideoController = new BMAP.VideoController(templateEl,resultEl);
-		
-		BMAP.LocalFeatures = new BMAP.LocalFeatures();
-		BMAP.SearchMachine = new BMAP.SearchMachine();
-		BMAP.PlaylistHandler = new BMAP.PlaylistHandler();
+		var templateEl = generateTemplate(),
+				resultEl   = $("#overlay-search-results"),
+
+				YoutubePlayer 		= new BMAP.YoutubePlayer(280,200),
+				VideoController 	= new BMAP.VideoController(templateEl,resultEl),
+				LocalFeatures 		= new BMAP.LocalFeatures(),
+				SearchMachine 		= new BMAP.SearchMachine(),
+				PlaylistHandler   = new BMAP.PlaylistHandler();
+
+		ko.applyBindings(PlaylistHandler);
 	});
-
-	var generateTemplate = function(){
-		var element = doc.createElement("div");
-		$(element).addClass("video");
-	
-		var playlist = doc.createElement("button");	
-		$(playlist).addClass("add add-to-playlist")
-		.html("Add")
-		.appendTo(element);
-		
-		var title = doc.createElement("h3");
-		$(title).addClass("title")
-		.appendTo(element);
-
-		return element;
-	};
 
 	win.BMAP = BMAP;
 })(window, document, jQuery);
