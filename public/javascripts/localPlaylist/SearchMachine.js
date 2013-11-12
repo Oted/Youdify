3c73fbb73f21589e0a0002ceb5701d2b07f16c88
Oted <herr.olle@gmail.com>
2013-11-12 16:32:54 +0100

    added functionallity for getting data from a single video id

--- public/javascripts/localPlaylist/SearchMachine.js
+++ public/javascripts/localPlaylist/SearchMachine.js
@@ -3,6 +3,7 @@
 	var BMAP = win.BMAP || {};
 	var PLAYLIST_QUERY_URL = "https://gdata.youtube.com/feeds/api/playlists/snippets?q=%s&v=2&alt=json";
 	var VIDEO_QUERY_URL = "http://gdata.youtube.com/feeds/api/videos?q=%s&max-results=50&v=2&alt=json"
+	var SINGLE_VIDEO_URL = "https://gdata.youtube.com/feeds/api/videos/%s?v=2&alt=json"
 
 	var SearchMachine = function(){
 		this.buildQueryUrl = function(url,query){
@@ -13,79 +14,95 @@
 			return url.replace(/\%s/, query);
 		};
 	};
-	
+
+	//called when need to harvest data from video id
+	SearchMachine.prototype.getVideoFromId = function(id, callback){
+		var that = this,
+			url = this.buildQueryUrl(SINGLE_VIDEO_URL, id);
+		
+		$.getJSON(url, function(data) {
+			console.log(data);
+			that.harvestData(data.entry, callback);
+		});
+	};
+
 	//the callback will fire when all videos are found, its called from videocontrollers keypress if radiobuttovideos is checked
 	SearchMachine.prototype.loadVideos = function(query, callback){
-		var url = this.buildQueryUrl(VIDEO_QUERY_URL, query);
+		var url = this.buildQueryUrl(VIDEO_QUERY_URL, query),
+			that = this;
+
 		$.getJSON(url, function(data) {
-			$.each(data.feed.entry, function(i, item) {
-				var entry = {};
-				
-				//containing playlist
-				entry.playlist = {};
-				
-				//id
-				entry.id = item.media$group.yt$videoid.$t;
-				
-				//title
-				entry.title = item.title.$t;
-				
-				//thumbnail
-				if (item.media$group && item.media$group.media$thumbnail){
-					entry.thumb = item.media$group.media$thumbnail[0].url;
-				}
-				else{
-					entry.thumb = "images/fallback_thumb.jpg";
-				}
-
-				//category of video
-				if (item.category[1] && item.category[1].term){
-					entry.category = item.category[1].term;
-				}
-				else{
-					entry.category = "-";
-				}
-				
-				//duration in minutes
-				if (item.media$group && item.media$group.yt$duration){
-					var seconds = item.media$group.yt$duration.seconds;
-					var min = Math.floor(seconds / 60); 
-					var remSec = (seconds % 60);
-					if (remSec===0){ 
-						entry.duration = min + ":00";
-					}
-					else{
-						entry.duration = min + ":" + remSec;
-					}
-				}
-				else{
-					entry.duration = "-";
-				}
-
-				//number of views
-				if (item.yt$statistics && item.yt$statistics.viewCount){
-					entry.views = item.yt$statistics.viewCount;
-				}
-				else{
-					entry.views = "-";
-				}
-				
-				//ratings and likes
-				if (item.yt$rating && item.yt$rating.numLikes){
-					entry.likes = item.yt$rating.numLikes;
-					entry.dislikes = item.yt$rating.numDislikes;
-					entry.average = Math.floor(entry.likes / entry.dislikes);
-				}
-				else{
-					entry.likes = "-";
-					entry.dislikes = "-";
-					entry.average = "-";
-				}
-
-				callback(entry);
+			$.each(data.feed.entry, function(i, item){
+				that.harvestData(item, callback);
 			});
-
 		});
+	};
+
+	SearchMachine.prototype.harvestData = function(item, callback){
+		var entry = {};
+				
+		//containing playlist
+		entry.playlist = {};
+					
+		//id
+		entry.id = item.media$group.yt$videoid.$t;
+				
+		//title
+		entry.title = item.title.$t;
+				
+		//thumbnail
+		if (item.media$group && item.media$group.media$thumbnail){
+			entry.thumb = item.media$group.media$thumbnail[0].url;
+		}
+		else{
+			entry.thumb = "images/fallback_thumb.jpg";
+		}
+
+		//category of video
+		if (item.category[1] && item.category[1].term){
+			entry.category = item.category[1].term;
+		}
+		else{
+			entry.category = "-";
+		}
+					
+		//duration in minutes
+		if (item.media$group && item.media$group.yt$duration){
+			var seconds = item.media$group.yt$duration.seconds;
+			var min = Math.floor(seconds / 60); 
+			var remSec = (seconds % 60);
+			if (remSec===0){ 
+				entry.duration = min + ":00";
+			}
+			else{
+				entry.duration = min + ":" + remSec;
+			}
+		}
+		else{
+			entry.duration = "-";
+		}
+
+		//number of views
+		if (item.yt$statistics && item.yt$statistics.viewCount){
+			entry.views = item.yt$statistics.viewCount;
+		}
+		else{
+			entry.views = "-";
+		}
+				
+		//ratings and likes
+		if (item.yt$rating && item.yt$rating.numLikes){
+			entry.likes = item.yt$rating.numLikes;
+			entry.dislikes = item.yt$rating.numDislikes;
+			entry.average = Math.floor(entry.likes / entry.dislikes);
+		}
+		else{
+			entry.likes = "-";
+			entry.dislikes = "-";
+			entry.average = "-";
+		}
+
+		callback(entry);
 	};
 
 	//the callback will fire when all playlists are found, its called from videocontrollers keypress if radiobutton playlist is checked
